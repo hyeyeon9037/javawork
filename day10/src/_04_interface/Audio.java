187a361214eec92dbcf1dcaf47058ef63d0b9687
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-11 10:14:54 +0900

    Update Audio.java

--- day10/src/_04_interface/Audio.java
+++ day10/src/_04_interface/Audio.java
@@ -1,38 +1,41 @@
 package _04_interface;
-
-public class Audio implements RemoteControl {
 /*
-	interface 
-	- 상속 받을 때 키워드는 implements
-	- 다중 상속 가능
-	*/
-	
-	private int volume;
-	
-	
-	@Override
-	public void turnOn() {
-		System.out.println("Audio를 킴");
+  -interface를 상속 받을 때 키워드는 implemnets
+  -다중상속 가능
+ */
 
-	}
+public class Audio implements RemoteControl /*, Inter, Face */{
+   private int volume;
+   
+   @Override
+   public void turnOn() {
+      System.out.println("Audio를 켭니다");
 
-	@Override
-	public void turnOff() {
-		System.out.println("Audio를 끔");
+   }
 
-	}
+   @Override
+   public void turnOff() {
+      System.out.println("Audio를 끕니다");
 
-	@Override
-	public void setVolume(int volume) {
-		if(volume > RemoteControl.MAX_VOLUME)
-			this.volume = RemoteControl.MAX_VOLUME;
-		else if (volume < RemoteControl.MIN_VOLUME)
-			this.volume = RemoteControl.MIN_VOLUME;
-	else
-		this.volume = volume;
-		System.out.println("현재 음량 : " + this.volume); //volume만 하면 지역변수임
-		
-	}
+   }
 
+   @Override
+   public void setVolume(int volume) {
+      if(volume > RemoteControl.MAX_VOLUME) 
+         this.volume = RemoteControl.MAX_VOLUME;
+      else if (volume < RemoteControl.MIN_VOLUME)
+         this.volume = RemoteControl.MIN_VOLUME;
+      else
+         this.volume = volume;
+      
+      System.out.println("현재 음량 : " + this.volume);
+
+   }
+
+   @Override
+   public void search1(String msg) {
+      // TODO Auto-generated method stub
+      
+   }
 
 }
