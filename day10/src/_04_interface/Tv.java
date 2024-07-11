8ba922420f9b3ccc069cf52e61c34b094dbb79a9
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-11 10:15:00 +0900

    Update Tv.java

--- day10/src/_04_interface/Tv.java
+++ day10/src/_04_interface/Tv.java
@@ -1,38 +1,45 @@
 package _04_interface;
+/*
+  -interface를 상속 받을 때 키워드는 implemnets
+  -다중상속 가능
+ */
 
 public class Tv implements RemoteControl {
-/*
-	interface 
-	- 상속 받을 때 키워드는 implements
-	- 다중 상속 가능
-	*/
-	
-	private int volume;
-	
-	
-	@Override
-	public void turnOn() {
-		System.out.println("Tv를 킴");
+   private int volume;
+   
+   @Override
+   public void turnOn() {
+      System.out.println("Tv를 켭니다");
 
-	}
+   }
 
-	@Override
-	public void turnOff() {
-		System.out.println("Tv를 끔");
+   @Override
+   public void turnOff() {
+      System.out.println("Tv를 끕니다");
 
-	}
+   }
 
-	@Override
-	public void setVolume(int volume) {
-		if(volume > RemoteControl.MAX_VOLUME)
-			this.volume = RemoteControl.MAX_VOLUME;
-		else if (volume < RemoteControl.MIN_VOLUME)
-			this.volume = RemoteControl.MIN_VOLUME;
-	else
-		this.volume = volume;
-		System.out.println("현재 음량 : " + this.volume); //volume만 하면 지역변수임
-		
-	}
-	
-	
+   @Override
+   public void setVolume(int volume) {
+      if(volume > RemoteControl.MAX_VOLUME) 
+         this.volume = RemoteControl.MAX_VOLUME;
+      else if (volume < RemoteControl.MIN_VOLUME)
+         this.volume = RemoteControl.MIN_VOLUME;
+      else
+         this.volume = volume;
+      
+      System.out.println("현재 음량 : " + this.volume);
+
+   }
+   @Override
+   public void search(String msg) {
+      System.out.println("msg");
+   }
+
+   @Override
+   public void search1(String msg) {
+      // TODO Auto-generated method stub
+      
+   }
+
 }
